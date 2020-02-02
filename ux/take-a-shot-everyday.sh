486379d3cdbb66d706ddcb8cbc57abc78332d08a
Stephane ERARD <serard06@protonmail.com>
2020-02-02 19:43:21 +0100

    fix scripts calling

--- ux/take-a-shot-everyday.sh
+++ ux/take-a-shot-everyday.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 args="$@"
 vagrant ssh -c "take-a-shot-everyday.ts shot ${args}"
-git.add.commit.sh "${args}"
+$dir/git.add.commit.sh "${args}"
